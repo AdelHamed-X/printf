4ab6b89ebf858b2a98ca42490fc6b017e148f80e
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-24 04:15:55 -0700

    Replacing the printf file

--- main.h
+++ main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <string.h>
 
+#include <stdarg.h>
+#include <stdio.h>
 
 int _putchar(char);
+int _putint(int);
 void _puts(char *);
 int _printf(const char *format, ...);
-int print_int(int value);
 
 #endif
