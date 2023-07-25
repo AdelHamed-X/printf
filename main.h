e60acb29eb4252e42d588aa56baf11eaa922c5b0
Salamne <saslmanesbm@gmail.com>
2023-07-25 06:14:21 -0700

    refix

--- main.h
+++ main.h
@@ -5,13 +5,10 @@
 #include <stddef.h>
 #include <string.h>
 
-int _strlenc(const char *s);
-int _strlen(char *s);
-void _flush_buffer(char *buffer, size_t size);
+
 int _putchar(char);
 int _puts(char *);
 int _printf(const char *format, ...);
 int _putint(int value);
-int _printpercent(void);
 
 #endif
