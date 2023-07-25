dc5604deb87a720259db752960ba5d93df8e6935
Salamne <saslmanesbm@gmail.com>
2023-07-25 12:02:15 -0700

    refix

--- print_string.c
+++ print_string.c
@@ -1,12 +1,8 @@
 #include "main.h"
 /**
  * print_string - Prints a string to the standard output (stdout).
-<<<<<<< HEAD
- * @:st The string to be printed.
-=======
+ * @:s The string to be printed.
  * @list: The string to be printed.
->>>>>>> bfaa3e9c12c8d2041bdfe6cdd87c9d704313268c
- *
  * Return: The number of characters printed.
  */
 
