4c76501a76fdf7167c512bdf60740e1e416f20f2
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-25 07:36:38 -0700

    fix

--- print_string.c
+++ print_string.c
@@ -1,40 +1,31 @@
 #include "main.h"
 /**
-<<<<<<< HEAD:_puts.c
  * _puts - Prints a string to the standard output (stdout).
  * @str: The string to be printed.
  *
  * Return: The number of characters printed.
  */
 
-int _puts(char *str)
-=======
- * print_string - print a string.
- * @s: argumen t.
- * Return: the length of the string.
- */
-
 int print_string(va_list list)
->>>>>>> Adel:print_string.c
 {
 	int i, len;
 	char *s;
 
 	s = va_arg(list, char *);
 
-	if (str == NULL)
+	if (s == NULL)
 	{
-		str = "(null)";
-		len = _strlen(str);
+		s = "(null)";
+		len = _strlen(s);
 		for (i = 0; i < len; i++)
-			_putchar(str[i]);
+			_putchar(s[i]);
 		return (len);
 	}
 	else
 	{
-		len = _strlen(str);
+		len = _strlen(s);
 		for (i = 0; i < len; i++)
-			_putchar(str[i]);
+			_putchar(s[i]);
 		return (len);
 	}
 
